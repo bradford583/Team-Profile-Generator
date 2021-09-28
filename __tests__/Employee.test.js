f5c48c0d9d278469d5b7313fa85e61b8e79929dc
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-27 22:01:19 -0600

    finished employee.js tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,3 +1,4 @@
+const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('Can instantiate Employee instance', () => {
@@ -15,5 +16,35 @@ test('Can set id via constructor arguments', () => {
     const testValue = 666;
     const e = new Employee("Foo", testValue);
     expect(e.id).toBe(testValue);
+});
+
+test('Can set email via constructor argurment', () => {
+    const testValue = "chris@chris.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test('Can get name via getName()', () => {
+    const testValue = "Chris";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test('Can get id via getId()', () => {
+    const testValue = 666;
+    const e = new Employee("Foo", testValue);
+    expect(e.getId()).toBe(testValue);
+});
+
+test('Can get email via getEmail()', () => {
+    const testValue = "chris@chris.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test('getRole() should return Employee', () => {
+    const testValue = "Employee";
+    const e = new Employee('Chris', 1, 'chris@chris.com');
+    expect(e.getRole()).toBe(testValue);
 });
 
