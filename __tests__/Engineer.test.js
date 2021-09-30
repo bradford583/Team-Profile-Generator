768e0192ef3df033228f3caf8c825087ef66af3a
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-29 22:59:43 -0600

    finished engineer tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer');
+
+test('Can set GitHub account via constructor', () => {
+    const testValue = 'GitHubUser';
+    const e = new Engineer("Foo", 1, "chris@chris.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
+test('getRole() returns Engineer', () => {
+    const testValue = "Engineer";
+    const e = new Engineer('Foo', 1, "chris@chris.com", 'GitHubUser');
+    expect(e.getRole()).toBe(testValue);
+});
+
+test('Can get username ver getGithub()', () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo", 1, "chris@chris.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+})
