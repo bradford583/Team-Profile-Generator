4e99b3464df59ed2a3cdbd11db1cb7b4d092072d
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-30 21:45:52 -0600

    added manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,19 @@
 const Manager = require('../lib/Manager');
+
+test("Can set office numebr via constructor argument", () => {
+    const testValue = 666;
+    const e = new Manager("Foo", 1, "chris@chris.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
+
+test("getRole() should return Manager", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "chris@chris.com");
+    expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+    const testValue = 100;
+    const e = new Manager("Foo", 1, "chris@chris.com", testValue);
+    expect(e.getOfficeNumber()).toBe(testValue);
+})
